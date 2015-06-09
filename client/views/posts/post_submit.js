4032f67da4136f7962ca8da076c275af49935a53
Diego de Souza <diegotrus7@gmail.com>
2015-06-08 23:10:49 -0400

    Removed insecure, only allowing logged in users to create posts.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,7 +6,7 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val(),
       message: $(e.target).find('[name=message]').val()
     };
-
+    // question to why we assign post._id on inserting
     post._id = Posts.insert(post);
     Router.go('postPage', post);
   }
--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,9 @@
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+  insert: function(userId, doc) {
+    // only allow inserting posts if logged in
+    return !! userId;
+  }
+});
+
