00d3340525f9a695178f08bb6e378c57806819e6
Diego de Souza <diegotrus7@gmail.com>
2015-06-08 23:03:49 -0400

    Added a submit post page and linked to it in the header .

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,7 +1,5 @@
 Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
+  message: function() {
+    return Posts.findOne({postId: this._id})
   }
 });
