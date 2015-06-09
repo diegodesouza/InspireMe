498a24fee80b4e17f21eff8734d66d92e20f33d6
Diego de Souza <diegotrus7@gmail.com>
2015-06-09 09:04:45 -0400

    Used a method to submit the post, add audit-argument checks.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -10,4 +10,12 @@ Template.postSubmit.events({
     post._id = Posts.insert(post);
     Router.go('postPage', post);
   }
+
+  Meteor.call('post', post, function(error, result) {
+    // display the error to the user and abort
+    if (error)
+      return alert(error.reason);
+
+    Router.go('postPage', {_id: result._id});
+  });
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,9 +1,24 @@
 Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-  insert: function(userId, doc) {
-    // only allow inserting posts if logged in
-    return !! userId;
+Meteor.methods({
+  postInsert: function(postAttributes) {
+    check(Meteor.userId(), String);
+    check(postAttributes, {
+      title: String,
+      message: String
+    });
+
+    var user = Meteor.user();
+    var post = _.extend(postAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date()
+    });
+
+    var postId = Posts.insert(post);
+
+    return {
+      _id: postId
+    };
   }
 });
-
