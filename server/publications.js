489357d78ae5fa9d4a3a0ada0c9519b784605945
Diego de Souza <diegotrus7@gmail.com>
2015-06-10 23:46:17 -0400

    Using a single post subscription to ensure that we can always see the right post.

--- server/publications.js
+++ server/publications.js
@@ -6,11 +6,16 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  check(id, String);
+  return Posts.find(id);
+});
+
 Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
 });
 
 Meteor.publish('notifications', function() {
-  return Notifications.find({userId: this.userId});
+  return Notifications.find({userId: this.userId, read: false});
 });
