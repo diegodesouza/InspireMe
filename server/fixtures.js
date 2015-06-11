24458f81a7627a4c6b8c36b0679f956fe7c016b1
Diego de Souza <diegotrus7@gmail.com>
2015-06-10 22:32:21 -0400

    Augmented the postList route to take a pagination(limit)/infinite load.

--- server/fixtures.js
+++ server/fixtures.js
@@ -36,15 +36,15 @@ if (Posts.find().count() === 0) {
     message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     submitted: now - 7 * 3600 * 1000
   });
-
+for (var i = 0; i < 20; i++) {
   Posts.insert({
-    title: "Something to be remembered",
+    title: "Something to be remembered" + 1,
     userId: jack._id,
     username: jack.profile.name,
-    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' + 1,
     submitted: now - 7 * 3600 * 1000
   });
-
+}
   Comments.insert({
     postId: inspirational,
     userId: diego._id,
