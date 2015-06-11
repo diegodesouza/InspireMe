b532c064965a8ef2b4a7ab3f8a5848828457ff63
Diego de Souza <diegotrus7@gmail.com>
2015-06-11 10:07:50 -0400

    Add votes, grey out upvoted like when not logged in and user has already voted.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,11 +1,19 @@
 Template.postItem.helpers({
   ownPost: function() {
     return this.userId == Meteor.userId();
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+     return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(event) {
+  'click .upvotable': function(event) {
     event.preventDefault();
     Meteor.call('upvote', this._id);
   }
